7103af71e5fe37f3a8dd132490dc05ae2f78c071
rogeralsing <rogeralsing@gmail.com>
2020-08-12 18:48:14 +0200

    .

--- hugo/themes/blank/assets/js/tabs.js
+++ hugo/themes/blank/assets/js/tabs.js
@@ -13,27 +13,30 @@ $(document).ready(function () {
         let li = $('<li class="nav-item"></li>').appendTo(navTabs);
         let a = $('<a class="nav-link" data-toggle="tab" role="tab" aria-selected="false" href="#' + id + '" aria-controls="' + id + '" >' + title + '</a>').appendTo(li);
 
-        a.click(() => {
-            if (inClick) {
-                return;
-            }
-            inClick = true;
-            let allTabLinks = $('.code-tabs').find("ul li a");
-            let allTabLinksOfSameTextExceptThis = allTabLinks.filter(function (index) { return $(this).text() === title && $(this).attr('href') != "#" + id })
-            allTabLinksOfSameTextExceptThis.click(); //this causes recursion on the click event
-            //I hack this using the inClick flag
-            inClick = false;
-        });
-
-
-        if (selected == title) { //first tab. activate it
-            $(this).addClass("active");
-            a
-                .addClass("active")
-                .attr("aria-selected", "true");
-        }
+        a.click(() => select(title));
 
         tabId++;
     });
+
+    select("C#");
 });
+
+
+function select(selected) {
+    $('.code-tabs').each(function () {
+        $(this).find("a").each(function () {
+            let a =  $(this);
+            let linkText = a.text();    
+            let isSelected = linkText === selected;
+            a.toggleClass("active",isSelected);
+            a.attr("aria-selected", isSelected);
+        });
+        $(this).find(".tab-pane").each(function () {
+            let tab =  $(this);
+            let tabText = tab.attr("title");    
+            let isSelected = tabText === selected;
+            tab.toggleClass("active",isSelected);
+        });
+    });
+}
 
