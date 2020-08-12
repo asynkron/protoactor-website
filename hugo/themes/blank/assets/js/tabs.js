bbfb826b0172e1c6240f23df30eb97c987a98782
rogeralsing <rogeralsing@gmail.com>
2020-08-12 17:13:10 +0200

    tabs

--- hugo/themes/blank/assets/js/tabs.js
+++ hugo/themes/blank/assets/js/tabs.js
@@ -1,4 +1,6 @@
 let tabId = 0;
+let selected="C#";
+
 $(document).ready(function () {
 
     $('.tab-content').each(function (inx2, tabContent) {
@@ -7,13 +9,21 @@ $(document).ready(function () {
             var navTabs = $(this).closest('.code-tabs').find('.nav-tabs');
             title = $(this).attr('title');
             $(this).attr("id", "tab" + tabId);
-            if (idx == 0) { //first tab. activate it
-                $(this).addClass("active");
-                navTabs.append('<li class="nav-item"><a class="nav-link active" data-toggle="tab" role="tab" aria-selected="true" href="#tab' + tabId + '" aria-controls="tab' + tabId + '" >' + title + '</a></li');
-            } else {
-                navTabs.append('<li class="nav-item"><a class="nav-link" data-toggle="tab" role="tab" aria-selected="false" href="#tab' + tabId + '" aria-controls="tab' + tabId + '" >' + title + '</a></li');
-            }
 
+            let a = $('<a class="nav-link" data-toggle="tab" role="tab" aria-selected="false" href="#tab' + tabId + '" aria-controls="tab' + tabId + '" >' + title + '</a>');
+            let li = $('<li class="nav-item"></li>');
+            a.appendTo(li);
+            li.appendTo(navTabs);
+
+
+            if (selected == title) { //first tab. activate it
+                $(this).addClass("active");
+                
+                a
+                .addClass("active")
+                .attr("aria-selected","true");
+            }
+            
             tabId++;
         });
     })
