d91b9fb733da977454372d0225060bf9a0faf269
rogeralsing <rogeralsing@gmail.com>
2020-08-12 18:50:44 +0200

    .

--- hugo/themes/blank/assets/js/tabs.js
+++ hugo/themes/blank/assets/js/tabs.js
@@ -3,12 +3,15 @@ let selected = "C#";
 let inClick = false;
 $(document).ready(function () {
 
+    //loop over each pane in the source
     $('.tab-content .tab-pane').each(function (idx, item) {
 
+        //get the nav tabs UL 
         var navTabs = $(this).closest('.code-tabs').find('.nav-tabs');
+        //get title from pane
         let title = $(this).attr('title');
         let id = 'tab' + tabId;
-        $(this).attr("id", "tab" + tabId);
+        $(this).attr("id", id);
 
         let li = $('<li class="nav-item"></li>').appendTo(navTabs);
         let a = $('<a class="nav-link" data-toggle="tab" role="tab" aria-selected="false" href="#' + id + '" aria-controls="' + id + '" >' + title + '</a>').appendTo(li);
@@ -21,22 +24,20 @@ $(document).ready(function () {
     select("C#");
 });
 
-
 function select(selected) {
     $('.code-tabs').each(function () {
         $(this).find("a").each(function () {
-            let a =  $(this);
-            let linkText = a.text();    
+            let a = $(this);
+            let linkText = a.text();
             let isSelected = linkText === selected;
-            a.toggleClass("active",isSelected);
+            a.toggleClass("active", isSelected);
             a.attr("aria-selected", isSelected);
         });
         $(this).find(".tab-pane").each(function () {
-            let tab =  $(this);
-            let tabText = tab.attr("title");    
+            let tab = $(this);
+            let tabText = tab.attr("title");
             let isSelected = tabText === selected;
-            tab.toggleClass("active",isSelected);
+            tab.toggleClass("active", isSelected);
         });
     });
 }
-
