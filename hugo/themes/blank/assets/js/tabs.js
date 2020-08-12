1ab2b42bea028347b32b66e3cb7876779118c10d
rogeralsing <rogeralsing@gmail.com>
2020-08-12 17:31:53 +0200

    dear lord what a hack

--- hugo/themes/blank/assets/js/tabs.js
+++ hugo/themes/blank/assets/js/tabs.js
@@ -1,24 +1,34 @@
 let tabId = 0;
 let selected="C#";
-
+let inClick = false;
 $(document).ready(function () {
 
     $('.tab-content').each(function (inx2, tabContent) {
         $(this).find('.tab-pane').each(function (idx, item) {
 
             var navTabs = $(this).closest('.code-tabs').find('.nav-tabs');
-            title = $(this).attr('title');
+            let title = $(this).attr('title');
+            let id = 'tab' + tabId;
             $(this).attr("id", "tab" + tabId);
 
-            let a = $('<a class="nav-link" data-toggle="tab" role="tab" aria-selected="false" href="#tab' + tabId + '" aria-controls="tab' + tabId + '" >' + title + '</a>');
+            let a = $('<a class="nav-link" data-toggle="tab" role="tab" aria-selected="false" href="#' + id + '" aria-controls="' + id + '" >' + title + '</a>');
             let li = $('<li class="nav-item"></li>');
             a.appendTo(li);
             li.appendTo(navTabs);
+            a.click(() => {
+                if (inClick){
+                    return;
+                }
+                inClick = true;
+                let allTabLinks = $('.code-tabs').find("ul li a");
+                let allTabLinksOfSameText = allTabLinks.filter(function(index) { return $(this).text() === title && $(this).attr('href') != "#" + id })
+                allTabLinksOfSameText.click();
+                inClick = false;
+            });
 
 
             if (selected == title) { //first tab. activate it
-                $(this).addClass("active");
-                
+                $(this).addClass("active");                
                 a
                 .addClass("active")
                 .attr("aria-selected","true");
