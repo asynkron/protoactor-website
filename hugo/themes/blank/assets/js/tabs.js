8937f734db99ab346d8982c05d2c66a0a68b902f
rogeralsing <rogeralsing@gmail.com>
2020-08-12 18:16:21 +0200

    .

--- hugo/themes/blank/assets/js/tabs.js
+++ hugo/themes/blank/assets/js/tabs.js
@@ -1,38 +1,39 @@
 let tabId = 0;
-let selected="C#";
+let selected = "C#";
 let inClick = false;
 $(document).ready(function () {
 
     $('.tab-content .tab-pane').each(function (idx, item) {
 
-            var navTabs = $(this).closest('.code-tabs').find('.nav-tabs');
-            let title = $(this).attr('title');
-            let id = 'tab' + tabId;
-            $(this).attr("id", "tab" + tabId);
+        var navTabs = $(this).closest('.code-tabs').find('.nav-tabs');
+        let title = $(this).attr('title');
+        let id = 'tab' + tabId;
+        $(this).attr("id", "tab" + tabId);
 
-            let li = $('<li class="nav-item"></li>').appendTo(navTabs);
-            let a = $('<a class="nav-link" data-toggle="tab" role="tab" aria-selected="false" href="#' + id + '" aria-controls="' + id + '" >' + title + '</a>').appendTo(li);            
-           
-            a.click(() => {
-                if (inClick){
-                    return;
-                }
-                inClick = true;
-                let allTabLinks = $('.code-tabs').find("ul li a");
-                let allTabLinksOfSameTextExceptThis = allTabLinks.filter(function(index) { return $(this).text() === title && $(this).attr('href') != "#" + id })
-                allTabLinksOfSameTextExceptThis.click(); //this causes recursion on the click event
-                //I hack this using the inClick flag
-                inClick = false;
-            });
+        let li = $('<li class="nav-item"></li>').appendTo(navTabs);
+        let a = $('<a class="nav-link" data-toggle="tab" role="tab" aria-selected="false" href="#' + id + '" aria-controls="' + id + '" >' + title + '</a>').appendTo(li);
 
-
-            if (selected == title) { //first tab. activate it
-                $(this).addClass("active");                
-                a
-                .addClass("active")
-                .attr("aria-selected","true");
+        a.click(() => {
+            if (inClick) {
+                return;
             }
-            
-            tabId++;
+            inClick = true;
+            let allTabLinks = $('.code-tabs').find("ul li a");
+            let allTabLinksOfSameTextExceptThis = allTabLinks.filter(function (index) { return $(this).text() === title && $(this).attr('href') != "#" + id })
+            allTabLinksOfSameTextExceptThis.click(); //this causes recursion on the click event
+            //I hack this using the inClick flag
+            inClick = false;
         });
+
+
+        if (selected == title) { //first tab. activate it
+            $(this).addClass("active");
+            a
+                .addClass("active")
+                .attr("aria-selected", "true");
+        }
+
+        tabId++;
     });
+});
+
